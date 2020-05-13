7191bc4312f46452fee883fb9b0cf9cfaf18da0d
Yueying <wangyy395@gmail.com>
2020-05-13 14:26:11 -0400

    rob & wyy done!

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,29 @@ class App extends React.Component {
     }
   }
 
+  changeFilterType = (event) => {
+    this.setState({
+      filters: {type: event.target.value}
+    })
+  }
+
+  fetchPetDate = () => {
+    let query = (this.state.filters.type === 'all' ? '':`?type=${this.state.filters.type}`)
+    fetch(`/api/pets${query}`)
+    .then(res => res.json())
+    .then(pets => this.setState({ pets }))
+    .catch(err => console.log('err: ', err))
+  }
+
+  toggleAdoption = (id) => {
+    const copy = this.state.pets.map(pet => pet.id === id ? {...pet, isAdopted: !pet.isAdopted } : pet)
+    this.setState({
+      pets: copy
+    })
+  }
+
   render() {
+    console.log('App state:', this.state)
     return (
       <div className="ui container">
         <header>
@@ -24,10 +46,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters fetchPetData={this.fetchPetDate} onChangeType={this.changeFilterType}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} toggleAdoption={this.toggleAdoption}/>
             </div>
           </div>
         </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -6,20 +6,26 @@ class Pet extends React.Component {
       <div className="card">
         <div className="content">
           <a className="header">
+            {this.props.gender === 'female'
+            ? '♀' 
+            : '♂'
+            }
             {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.props.isAdopted
+          ? <button className="ui disabled button"  onClick={() => this.props.toggleAdoption(this.props.id)} >Already adopted</button>
+          : <button className="ui primary button" onClick={() => this.props.toggleAdoption(this.props.id)} >Adopt pet</button>
+          }
         </div>
       </div>
     )
